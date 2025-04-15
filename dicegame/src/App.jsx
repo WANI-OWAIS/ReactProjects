15acc54daf80a40f8bd16f07fb7e00903e41ab01
WANI-OWAIS <waniowais528@gmail.com>
2025-04-16 01:08:37 +0530

    projectCompleted

--- dicegame/src/App.jsx
+++ dicegame/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useState } from "react";
+import StartGame from "./components/StartGame";
+import GamePlay from "./components/GamePlay";
 
 function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  };
+
   return (
-    <div>App</div>
-  )
+    <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}</>
+  );
 }
 
-export default App
+export default App;
